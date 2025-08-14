9079d80aaf1a9c63749e7f1297578cf5f2e20877
SambitParida <parida.sambit@gmail.com>
2025-08-14 16:21:57 -0400

    [skip ci]

--- deploy/V1.1.2__Create_csv_file_format.sql
+++ deploy/V1.1.2__Create_csv_file_format.sql
@@ -5,20 +5,4 @@ use database ttips;
 -- use schema --
 use schema ch01;
 -- create transient table --
-create or replace transient table customer_csv (
-	customer_pk number(38,0),
-	salutation varchar(10),
-	first_name varchar(20),
-	last_name varchar(30),
-	gender varchar(1),
-	marital_status varchar(1),
-	day_of_birth date,
-	birth_country varchar(60),
-	email_address varchar(50),
-	city_name varchar(60),
-	zip_code varchar(10),
-	country_name varchar(20),
-	gmt_timezone_offset number(10,2),
-	preferred_cust_flag boolean,
-	registration_time timestamp_ltz(9)
-);
+create or replace file format customer_csv_ff type = 'csv' compression = 'auto' field_delimiter = ',' record_delimiter = '\n' skip_header = 1 field_optionally_enclosed_by = '"' null_if = ('\\n');
