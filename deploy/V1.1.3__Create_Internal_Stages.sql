07971199ef6cd9b6547af122755876debe2d12ec
SambitParida <parida.sambit@gmail.com>
2025-08-15 13:31:43 -0400

    [skip ci]

--- deploy/V1.1.3__Create_Internal_Stages.sql
+++ deploy/V1.1.3__Create_Internal_Stages.sql
@@ -8,3 +8,5 @@ use schema ch01;
 -- create transient t
 
 create stage if not exists ch01.csv_stg directory = (enable = true) comment = 'this is a snowflake internal stage';
+create stage if not exists ch01.psv_stg directory = (enable = true) comment = 'this is a snowflake internal stage to store data from pipe separated file';
+create stage if not exists ch01.tsv_stg directory = (enable = true) comment = 'this is a snowflake internal stage to store data from tab separated file';
--- execute_statements.sql
+++ execute_statements.sql
@@ -46,8 +46,48 @@ from @ttips.ch01.csv_stg (file_format => 'ttips.ch01.customer_csv_ff') t;
 PUT file:///Users/sambitparida/Desktop/Sambit/Learning/SnowflakePractice/data/DataIngestion/ch01/customer_10k_good_data.tsv @tsv_stg;
 list @tsv_stg;
 
-
+select t.$1::text as customer_pk,
+    t.$2::text as salutation,
+    t.$3::text as first_name,
+    t.$4::text as last_name,
+    t.$5::text as gender,
+    t.$6::text as marital_status,
+    t.$7::text as day_of_birth,
+    t.$8::text as birth_country,
+    t.$9::text as email_address,
+    t.$10::text as city_name,
+    t.$11::text as zip_code,
+    t.$12::text as country_name,
+    t.$13::text as gmt_timezone_offset,
+    t.$14::text as preferred_cust_flag,
+    t.$15::text as registration_time,
+    metadata$filename as stg_file_name,
+    metadata$file_last_modified as stg_file_load_ts,
+    metadata$file_content_key as stg_file_md5,
+    current_timestamp as copy_data_ts
+from @ttips.ch01.tsv_stg (file_format => 'ttips.ch01.customer_tsv_ff') t;
 
 -- Load psv data from local directory to snowflake internal named stage and verify --
 PUT file:///Users/sambitparida/Desktop/Sambit/Learning/SnowflakePractice/data/DataIngestion/ch01/customer_10k_good_data.psv @psv_stg;
 list @psv_stg;
+
+select t.$1::text as customer_pk,
+    t.$2::text as salutation,
+    t.$3::text as first_name,
+    t.$4::text as last_name,
+    t.$5::text as gender,
+    t.$6::text as marital_status,
+    t.$7::text as day_of_birth,
+    t.$8::text as birth_country,
+    t.$9::text as email_address,
+    t.$10::text as city_name,
+    t.$11::text as zip_code,
+    t.$12::text as country_name,
+    t.$13::text as gmt_timezone_offset,
+    t.$14::text as preferred_cust_flag,
+    t.$15::text as registration_time,
+    metadata$filename as stg_file_name,
+    metadata$file_last_modified as stg_file_load_ts,
+    metadata$file_content_key as stg_file_md5,
+    current_timestamp as copy_data_ts
+from @ttips.ch01.psv_stg (file_format => 'ttips.ch01.customer_psv_ff') t;
