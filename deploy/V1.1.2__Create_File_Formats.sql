d38b5c3d906e81f052941d334a7fa49f690399e9
SambitParida <parida.sambit@gmail.com>
2025-08-15 14:11:07 -0400

    [skip ci]

--- deploy/V1.1.2__Create_File_Formats.sql
+++ deploy/V1.1.2__Create_File_Formats.sql
@@ -2,17 +2,38 @@
 use role sysadmin;
 -- use virtual warehouse -- 
 use warehouse data_ingestion_wh;
-
 -- use database --
 use database ttips;
 -- use schema --
 use schema ch01;
-
 -- Create CSV File format -- 
-create or replace file format customer_csv_ff type = 'csv' compression = 'auto' field_delimiter = ',' record_delimiter = '\n' skip_header = 1 field_optionally_enclosed_by = '"' null_if = ('\\n','\\N','');
+create or replace file format customer_csv_ff 
+    type = 'csv' 
+    compression = 'auto' 
+    field_delimiter = ',' 
+    record_delimiter = '\n' 
+    date_format = 'AUTO'
+    timestamp_format = 'AUTO'
+    skip_header = 1 
+    field_optionally_enclosed_by = '"' 
+    null_if = ('\\n', '\\N', '');
 
 -- Create TSV File format -- 
-create or replace file format customer_tsv_ff type = 'csv' compression = 'auto' field_delimiter = '\t' record_delimiter = '\n' skip_header = 1 field_optionally_enclosed_by = '"' null_if = ('\\n','\\N','');
+create or replace file format customer_tsv_ff 
+    type = 'csv' 
+    compression = 'auto' 
+    field_delimiter = '\t' 
+    record_delimiter = '\n' 
+    skip_header = 1 
+    field_optionally_enclosed_by = '"' 
+    null_if = ('\\n', '\\N', '');
 
 -- Create PSV File format -- 
-create or replace file format customer_psv_ff type = 'csv' compression = 'auto' field_delimiter = '|' record_delimiter = '\n' skip_header = 1 field_optionally_enclosed_by = '"' null_if = ('\\n','\\N','');
+create or replace file format customer_psv_ff 
+    type = 'csv' 
+    compression = 'auto' 
+    field_delimiter = '|' 
+    record_delimiter = '\n' 
+    skip_header = 1 
+    field_optionally_enclosed_by = '"' 
+    null_if = ('\\n', '\\N', '');
--- execute_statements.sql
+++ execute_statements.sql
@@ -7,20 +7,6 @@ use schema ch01;
 PUT file:///Users/sambitparida/Desktop/Sambit/Learning/SnowflakePractice/data/DataIngestion/ch01/customer_10k_good_data.csv @csv_stg;
 
 list @csv_stg;
-select t.$1::text as orderid -- t.$2::text as customerid,
-    -- t.$3::text as restaurantid,
-    -- t.$4::text as orderdate,
-    -- t.$5::text as totalamount,
-    -- t.$6::text as status,
-    -- t.$7::text as paymentmethod,
-    -- t.$8::text as createddate,
-    -- t.$9::text as modifieddate,
-    -- metadata $filename as stg_file_name,
-    -- metadata $file_last_modified as stg_file_load_ts,
-    -- metadata $file_content_key as stg_file_md5,
-    -- current_timestamp as copy_data_ts
-from @ttips.ch01.csv_stg t (FILE_FORMAT => 'ttips.ch01.customer_csv_ff');
-
 select t.$1::text as customer_pk,
     t.$2::text as salutation,
     t.$3::text as first_name,
@@ -91,3 +77,59 @@ select t.$1::text as customer_pk,
     metadata$file_content_key as stg_file_md5,
     current_timestamp as copy_data_ts
 from @ttips.ch01.psv_stg (file_format => 'ttips.ch01.customer_psv_ff') t;
+
+
+-- Check Load History for status --
+select * from information_schema.load_history
+
+-- Load csv data from local directory to snowflake internal named stage and verify : ERRORNEOUS FILE with wrong boolean data --
+PUT file:///Users/sambitparida/Desktop/Sambit/Learning/SnowflakePractice/data/DataIngestion/ch01/data-file-with-2-issues.csv @csv_stg;
+
+list @csv_stg;
+select t.$1::text as customer_pk,
+    t.$2::text as salutation,
+    t.$3::text as first_name,
+    t.$4::text as last_name,
+    t.$5::text as gender,
+    t.$6::text as marital_status,
+    t.$7::text as day_of_birth,
+    t.$8::text as birth_country,
+    t.$9::text as email_address,
+    t.$10::text as city_name,
+    t.$11::text as zip_code,
+    t.$12::text as country_name,
+    t.$13::text as gmt_timezone_offset,
+    t.$14::text as preferred_cust_flag,
+    t.$15::text as registration_time,
+    metadata$filename as stg_file_name,
+    metadata$file_last_modified as stg_file_load_ts,
+    metadata$file_content_key as stg_file_md5,
+    current_timestamp as copy_data_ts
+from @ttips.ch01.csv_stg/data-file-with-2-issues.csv (file_format => 'ttips.ch01.customer_csv_ff') t;
+
+
+-- Load csv data from local directory to snowflake internal named stage and verify : ERRORNEOUS FILE with wrong data length --
+
+PUT file:///Users/sambitparida/Desktop/Sambit/Learning/SnowflakePractice/data/DataIngestion/ch01/data-file-with-datatype-issue.csv @csv_stg;
+
+list @csv_stg;
+select t.$1::text as customer_pk,
+    t.$2::text as salutation,
+    t.$3::text as first_name,
+    t.$4::text as last_name,
+    t.$5::text as gender,
+    t.$6::text as marital_status,
+    t.$7::text as day_of_birth,
+    t.$8::text as birth_country,
+    t.$9::text as email_address,
+    t.$10::text as city_name,
+    t.$11::text as zip_code,
+    t.$12::text as country_name,
+    t.$13::text as gmt_timezone_offset,
+    t.$14::text as preferred_cust_flag,
+    t.$15::text as registration_time,
+    metadata$filename as stg_file_name,
+    metadata$file_last_modified as stg_file_load_ts,
+    metadata$file_content_key as stg_file_md5,
+    current_timestamp as copy_data_ts
+from @ttips.ch01.csv_stg/data-file-with-2-issues.csv (file_format => 'ttips.ch01.customer_csv_ff') t;
